c295f6d1bb2bd689aa87f1ee25ae9097161378d8
Pascal Ablitzer <pascal.ablitzer@gmail.com>
2022-02-10 17:44:50 +0100

    fix build

--- script/build.sh
+++ script/build.sh
@@ -2,5 +2,6 @@ if [ -d $INIT_CWD/lib ]
 then
   rm -r $INIT_CWD/lib && mkdir $INIT_CWD/lib
 fi
+mkdir $INIT_CWD/lib
 cp -R $INIT_CWD/node_modules/bootstrap-icons $INIT_CWD/lib/bootstrap-icons;
 cp -R $INIT_CWD/node_modules/jquery $INIT_CWD/lib/jquery;
